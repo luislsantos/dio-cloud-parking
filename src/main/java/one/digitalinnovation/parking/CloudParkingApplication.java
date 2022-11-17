1f63ce4ce6ec025654bcaa654f8b5391ad303ccd
luisantoniosantos <luislessa@outlook.com>
2022-11-17 20:14:02 -0300

    Adicionando swagger

--- src/main/java/one/digitalinnovation/parking/CloudParkingApplication.java
+++ src/main/java/one/digitalinnovation/parking/CloudParkingApplication.java
@@ -2,6 +2,7 @@ package one.digitalinnovation.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class CloudParkingApplication {
